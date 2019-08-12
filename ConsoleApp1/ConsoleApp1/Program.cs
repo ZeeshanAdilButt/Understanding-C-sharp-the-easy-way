b597a11ed691e86fb435acb7136ef73b0dd810a8
Zeeshan <zeeabutt@outlook.com>
2019-08-12 19:02:40 +0500

    Implemented Delegate example with Func delegate and paramterized our code.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    //this is a delegate which gets turned into a class on MSIL level
+    //seems just like a method definition 😶
+    delegate void MyDelegate();
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Delegates!");
+
+            //look at the constructor here: It takes a method
+            MyDelegate myDelegate = new MyDelegate(MyXMethod);
+
+            //so you said it is a class on complier level but we are treating it as a method here? eh? 🙄
+            //Yes! that is just a syntatic sugar where the complier actually calls myDelegate.Invoke()😁
+            myDelegate();
+            Console.WriteLine(myDelegate.Method);
+            Console.WriteLine(myDelegate.Target);
+
+            // myDelegate.Invoke(); -- this will work too!
+
+
+        }
+
+        static void MyXMethod() {
+
+            Console.WriteLine("hello from MyXMethod");
+        }
+
+        void MyYMethod()
+        {
+            Console.WriteLine("hello from MyXMethod");
+        }
+    }
+}
---
+++ ConsoleApp1/DelegateCalculatorExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DelegateCalculatorExample
+{
+
+    //delegate that returns true or false depending upon the passed number and expression
+    //delegate bool NumberComparerDelegate(int n); --> no need for this now
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var sampleArray = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Console.WriteLine("Running FilterNumbers");
+            IEnumerable<int> result2 = FilterNumbers(sampleArray, n => n <5);
+
+            foreach (int num in result2)
+            {
+                Console.WriteLine(num);
+            }
+
+        }
+
+        private static IEnumerable<int> FilterNumbers(IEnumerable<int> sampleArray, Func<int, bool> numberComparer)
+        {
+            foreach (int number in sampleArray)
+            {
+                if (numberComparer(number))
+                    yield return number;
+            }
+        }
+
+    }
+}
